43fe9d5fda43100fc352bff1e8b0986cdb24a337
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2019-11-21 11:50:25 +0530

    Jobs app up and running

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'vaibhav',
+        'PASSWORD':'vaibhav123',
+        'HOST':'127.0.0.1',
     }
 }
 
