fca2a3aecbc467dae694a39b1cd35dc62ed3c27f
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2019-11-21 14:38:20 +0530

    added bootsrap and job successfully

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+
+    pub_date = models.DateTimeField()
+
+    body = models.TextField()
+
+    image = models.ImageField(upload_to='images/')
+
+
+# Create A Blog model
+
+# Add the blog app to the setting
+
+# Create migration
+
+# migrate
+
+# add to admin
